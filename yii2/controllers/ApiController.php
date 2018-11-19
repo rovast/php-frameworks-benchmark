2bfd3a7b4769ce47fd12decd2e139f823443ffc4
rovast <rovast@163.com>
2018-11-19 13:36:04 +0800

    🎨 add api/hello

---
+++ yii2/controllers/ApiController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Class ApiController
+ */
+class ApiController extends Controller
+{
+    public function actionHello()
+    {
+        echo 'hello world';
+    }
+}
