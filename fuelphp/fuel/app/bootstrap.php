c6ebcf0cbf3caf427271bccf9823db1926ae2e27
rovast <rovast@163.com>
2018-11-21 16:39:21 +0800

    🎉 add fulephp

---
+++ fuelphp/fuel/app/bootstrap.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP 5.4+ framework.
+ *
+ * @package    Fuel
+ * @version    1.8.1
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2018 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+// Bootstrap the framework DO NOT edit this
+require COREPATH.'bootstrap.php';
+
+\Autoloader::add_classes(array(
+	// Add classes you want to override here
+	// Example: 'View' => APPPATH.'classes/view.php',
+));
+
+// Register the autoloader
+\Autoloader::register();
+
+/**
+ * Your environment.  Can be set to any of the following:
+ *
+ * Fuel::DEVELOPMENT
+ * Fuel::TEST
+ * Fuel::STAGING
+ * Fuel::PRODUCTION
+ */
+\Fuel::$env = \Arr::get($_SERVER, 'FUEL_ENV', \Arr::get($_ENV, 'FUEL_ENV', getenv('FUEL_ENV') ?: \Fuel::DEVELOPMENT));
+
+// Initialize the framework with the config file.
+\Fuel::init('config.php');
