621f435c1a7b25d1a129cfd822cb5e55cb710740
rovast <rovast@163.com>
2018-11-23 11:13:25 +0800

    ⚗ add xhprof config

--- php/xhprof.php
+++ php/xhprof.php
@@ -1,5 +1,8 @@
 <?php
+
 require_once "/var/www/html/xhgui/external/header.php";
+
+\xhprof_enable(XHPROF_FLAGS_MEMORY | XHPROF_FLAGS_CPU);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -22,4 +25,20 @@ $capsule->setAsGlobal();
 
 $result = Capsule::table('test')->first()->name;
 
-echo $result;
+
+
+// stop profiler
+$xhprof_data = xhprof_disable();
+
+// display raw xhprof data for the profiler run
+print_r($xhprof_data);
+
+include_once __DIR__ . '/xhprof_lib.php';
+include_once __DIR__ . '/xhprof_runs.php';
+
+// save raw data for this profiler run using default
+// implementation of iXHProfRuns.
+$xhprof_runs = new XHProfRuns_Default();
+
+// save the run under a namespace "xhprof_foo"
+$run_id = $xhprof_runs->save_run($xhprof_data, 'php-orm');
